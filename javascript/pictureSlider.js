dc4d7b8dc8407934e14eb48a769ea8281332a136
Marc <phyrenight@gmail.com>
2017-06-22 12:53:40 -0400

    add slider

--- javascript/pictureSlider.js
+++ javascript/pictureSlider.js
@@ -1,19 +1,23 @@
 // Javascript for the slider
 
-var pictures = ["a", "b","c", "d"];
+var pictures = ["one.jpg", "two.jpg","three.jpg", "four.jpg"];
 var currentStatus = {
 	picture: "",
 	slideNumber: 0
 };
 
-//setInterval(changeSlider, 1000);
+setInterval(changeSlider, 3000);
 
 function changeSlider(){
-	currentStatus.slideNumber += 1
+	var slideView = document.getElementById("slide");
 	var picturesLength = pictures.length;
-	if(currentStatus.slideNumber >= picturesLength){
+	if(currentStatus.slideNumber >= (picturesLength-1)){
 		currentStatus.slideNumber = 0;
 	}
-    console.log(pictures[currentStatus.slideNumber])    
+	else{
+		currentStatus.slideNumber += 1;
+	}
+    slide.style.backgroundImage = "url('images/"+pictures[currentStatus.slideNumber]+"')";   
 
 }
+
